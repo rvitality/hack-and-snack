4f22946cc4a8764683632e176c52254326064e0f
rvitality <vital.renzjohn@gmail.com>
2022-10-09 07:43:25 +0800

    feat: allow user to log in with deso

--- frontend/src/components/Header/Header.component.jsx
+++ frontend/src/components/Header/Header.component.jsx
@@ -1,48 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Link, Outlet } from "react-router-dom";
+
+import Deso from "deso-protocol";
+const deso = new Deso();
+
+import logo from "../../assets/images/logo.png";
 
 import { AiFillFacebook, AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
 import { BiUserCircle } from "react-icons/bi";
 import { BsMouse } from "react-icons/bs";
+import { useAuthContext } from "../../context/AuthContext";
 
 const Header = () => {
+    const { login, logout, isLoggedIn } = useAuthContext();
+
+    const [loginResponse, setLoginResponse] = useState();
+
+    const loginHandler = async () => {
+        const { user } = await deso.identity.login();
+
+        if (Object.keys(user).length === 0) {
+            return;
+        }
+
+        const { encryptedSeedHex } = user;
+
+        if (!encryptedSeedHex) return;
+
+        setLoginResponse(encryptedSeedHex);
+
+        // context, set user data
+        login(encryptedSeedHex);
+    };
+
     return (
-        <header className="absolute z-[99] top-0 left-0 right-0 p-8 px-16">
-            <nav className="flex justify-between font-semibold w-1/2 pr-12">
-                <ul className="flex space-x-5">
-                    <li>
+        <>
+            {/* <header
+                className={`${
+                    !isLoggedIn ? "absolute" : "relative"
+                } z-[99] top-0 left-0 right-0 p-4 px-6`}
+            > */}
+            <header className={` z-[99] top-0 left-0 right-0 p-4 px-6`}>
+                <nav
+                    className={`flex items-center justify-between font-semibold  pr-12 ${
+                        !isLoggedIn ? "w-1/2" : ""
+                    }`}
+                >
+                    <div className="flex items-center ">
                         <a href="#">
-                            <AiFillFacebook className="h-6 w-6" />
+                            <img src={logo} alt="" className="w-[6rem] mr-6" />
                         </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <AiOutlineInstagram className="h-6 w-6" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <AiOutlineTwitter className="h-6 w-6" />
-                        </a>
-                    </li>
-                </ul>
 
-                <ul className="flex space-x-5">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Recipes</a>
-                    </li>
-                    <li>
-                        <a href="#">Match</a>
-                    </li>
-                </ul>
+                        {/* <ul className="flex space-x-5">
+                            <li>
+                                <a href="#" className="duration-200 text-gray hover:text-black">
+                                    <AiFillFacebook className="h-6 w-6" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="duration-200 text-gray hover:text-black">
+                                    <AiOutlineInstagram className="h-6 w-6" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className="duration-200 text-gray hover:text-black">
+                                    <AiOutlineTwitter className="h-6 w-6" />
+                                </a>
+                            </li>
+                        </ul> */}
+                    </div>
 
-                <a href="#">
-                    <BiUserCircle className="h-7 w-7" />
-                </a>
-            </nav>
-        </header>
+                    {/* {isLoggedIn && (
+                        <ul className="flex space-x-5">
+                            <li>
+                                <Link to="/" className="duration-200 text-gray hover:text-black">
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="#" className="duration-200 text-gray hover:text-black">
+                                    Recipes
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href="/feed"
+                                    className="duration-200 text-gray hover:text-black"
+                                >
+                                    Feed
+                                </Link>
+                            </li>
+                        </ul>
+                    )} */}
+
+                    {!isLoggedIn && (
+                        <a
+                            href="#"
+                            className=" border-[1px] text-gray border-gray rounded-full px-6 py-2 duration-150 hover:bg-black hover:text-white"
+                            onClick={loginHandler}
+                        >
+                            Login
+                        </a>
+                    )}
+
+                    {isLoggedIn && (
+                        <Link
+                            to="/"
+                            className=" border-[1px] border-primary bg-primary rounded-full px-6 py-2 duration-200 hover:border-black hover:brightness-105"
+                            onClick={logout}
+                        >
+                            Log Out
+                        </Link>
+                    )}
+                </nav>
+            </header>
+
+            <main>
+                <Outlet />
+            </main>
+        </>
     );
 };
 
