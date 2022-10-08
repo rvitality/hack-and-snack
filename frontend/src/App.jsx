5cc3f8dfe93b75c195369e6d781e31fa96e210e9
rvitality <vital.renzjohn@gmail.com>
2022-10-08 17:55:25 +0800

    feat: add hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import watermelon from "./assets/images/watermelon.png";
+import phone from "./assets/images/phone.png";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+import "./App.css";
 
-export default App
+const App = () => {
+    return (
+        <div className="relative overflow-hidden">
+            <div className="flex h-screen justify-end">
+                {/* LEFT SIDE */}
+                <div className="hero-left flex items-center justify-center w-1/2 ">
+                    <img src={phone} alt="" className="h-3/4 mr-14 mt-20 z-[2]" />
+
+                    <h1 className="main-heading absolute left-[100%] top-[40%] z-[5] flex flex-col items-start  font-satisfy text-[5rem] leading-none">
+                        <span className="">choose</span>
+                        <span className="text-[2.5rem]">your</span>
+                        <span className="text-[7rem] ml-[5rem] mt-[-5rem]">match</span>
+                    </h1>
+                </div>
+
+                {/* RIGHT SIDE */}
+                <div className="relative w-[55%] hero-right flex items-end ">
+                    <img
+                        src={watermelon}
+                        alt=""
+                        className="melon absolute z-[2] h-80 object-contain top-[55%] left-[5%]"
+                    />
+
+                    <div className="large-text__right ">
+                        <span className="text-[5rem] block -mb-8">and find</span>{" "}
+                        <span className="ml-4">good</span> <span className="ml-28">food</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
